038de00f3f03706d0afdf0a3da3315c8d502c2ce
Denis Otkidach <denis.otkidach@gmail.com>
2018-11-12 11:59:11 +0300

    Make it installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    setup(
+        name='async_ready',
+        description='Primitives to simplify writing code that works in both '
+                    'sync and async environments',
+        license='MIT license',
+        python_requires='>=3.6',
+        py_modules=['async_ready'],
+    )
